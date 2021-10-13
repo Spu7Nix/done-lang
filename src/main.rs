becb12743dc7c1c74c9c837f95e27a80c88ba421
Spu7Nix <spu7nix@outlook.com>
2021-10-13 11:34:10 +0200

    parsing??

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use crate::parser::parse_mod;
+
 mod interpreter;
 mod parser;
 
 fn main() {
-    dbg!(parser::lex(
-        "number is whole if the number is equal to the number rounded."
+    let lexed = dbg!(parser::lex(
+        "number rounded up is 10."
     ));
+
+    let parsed = dbg!(parser::parse_mod(&lexed));
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use logos::Logos;
 
 #[derive(Logos, Debug, PartialEq, Eq)]
@@ -27,7 +29,10 @@ enum LogosToken {
     #[regex(r#"[a-z]?"(?:\\.|[^\\"])*"|'(?:\\.|[^\\'])*'"#)]
     StringLiteral,
 
-    #[regex(r#"\w+"#, priority = 0)]
+    #[regex(r#"([A-Z]\w*)|number|string|list"#, priority = 1)]
+    TypeName,
+
+    #[regex(r#"[^A-Z\W]\w*"#, priority = 0)]
     Symbol,
 
     #[error]
@@ -46,6 +51,7 @@ pub enum Token<'a> {
     Number(f64),
     StringLiteral(&'a str),
     Symbol(&'a str),
+    TypeName(&'a str),
 }
 
 pub fn lex(text: &str) -> Vec<Token> {
@@ -54,7 +60,7 @@ pub fn lex(text: &str) -> Vec<Token> {
     while let Some(token) = iter.next() {
         tokens.push(match token {
             LogosToken::The => Token::The({
-                assert_eq!(iter.next().unwrap(), LogosToken::Symbol);
+                assert_eq!(iter.next().unwrap(), LogosToken::TypeName);
                 iter.slice()
             }),
             LogosToken::If => Token::If,
@@ -63,10 +69,94 @@ pub fn lex(text: &str) -> Vec<Token> {
             LogosToken::Define => Token::Define,
             LogosToken::Comma => Token::Comma,
             LogosToken::Number => Token::Number(iter.slice().parse().unwrap()),
-            LogosToken::StringLiteral => Token::StringLiteral(iter.slice()),
+            LogosToken::StringLiteral => Token::StringLiteral({
+                let slice = iter.slice();
+                &slice[1..(slice.len() - 1)]
+            }),
             LogosToken::Symbol => Token::Symbol(iter.slice()),
             LogosToken::Error => todo!(),
+            LogosToken::TypeName => Token::TypeName(iter.slice()),
         })
     }
     tokens
+}
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum Expr {
+    Number(f64),
+    Str(String),
+    Call {
+        func: FuncSignature,
+        args: Vec<Expr>
+    },
+    The(String)
+}
+
+
+
+#[derive(PartialEq, Eq, Hash, Clone, Debug)]
+pub enum FuncSymbol {
+    Argument,
+    Symbol(String)
+}
+
+#[derive(PartialEq, Eq, Hash, Clone, Debug)]
+pub struct FuncSignature(Vec<FuncSymbol>);
+#[derive(Debug)]
+pub struct Functions(HashMap<FuncSignature, Expr>);
+
+pub fn parse_mod(tokens: &[Token]) -> Functions {
+    let mut funcs = Functions(HashMap::new());
+    let sentences = tokens.split(|a| *a == Token::Period);
+    for s in sentences {
+        if !s.is_empty() {
+            parse_sentence(s, &mut funcs);
+        }
+    }
+    funcs
+}
+
+fn split_at_first<'a>(tokens: &'a [Token], t: Token) -> (&'a [Token<'a>], &'a [Token<'a>]) {
+    let out = tokens.split_at(tokens.iter().position(|a | *a == t).unwrap());
+    (out.0, &out.1[1..])
+}
+
+fn parse_sentence(s: &[Token], funcs: &mut Functions) {
+    let (def_part, main_part) = split_at_first(s, Token::Is);
+
+    match (def_part.len(), def_part[0]) {
+        (1, Token::TypeName(ty)) => {
+            todo!();
+            // pattern def
+            let (pattern, condition) = split_at_first(s, Token::If);
+        },
+        _ => {
+            // func
+            let signature = parse_multi_word_def(def_part);
+            let f = parse_expr(main_part, funcs);
+            funcs.0.insert(signature.clone(), f);
+        }
+    }
+}
+
+fn parse_multi_word_def<'a>(s: &'a [Token]) -> FuncSignature {
+    FuncSignature(s.iter().map(|a| match a {
+        Token::Symbol(s) => FuncSymbol::Symbol(s.to_string()),
+        Token::TypeName(_) => FuncSymbol::Argument,
+        _ => panic!("unexpected")
+    }).collect())
+}
+
+
+
+fn parse_expr<'a>(s: &'a [Token], funcs: &'a Functions) -> Expr {
+    
+    match s[0] {
+        Token::The(a) => Expr::The(a.to_string()),
+        Token::Number(n) => Expr::Number(n),
+        Token::StringLiteral(s) => Expr::Str(s.to_string()),
+        Token::Symbol(_) => todo!(),
+        Token::TypeName(_) => todo!(),
+        a => panic!("unexpected {:?}", a)
+    }
 }
